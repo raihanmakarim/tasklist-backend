b6f30c5c4974a478a2d19ab55a38d25c345d09b3
raihanmakarim <111282057+raihanmakarim@users.noreply.github.com>
2024-05-30 22:43:04 +0700

    feat: add task entity

---
+++ README.md
@@ -0,0 +1,1 @@
+# test-hyundai-backend
---
+++ test-hyundai-backend/Entities/Task.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace test_hyundai_backend.Entities
+{
+	public class Task
+	{
+		public int Id { get; set; }
+		public required string Title { get; set; }
+		public string desc { get; set; } = string.Empty;
+
+
+	}
+}
+
